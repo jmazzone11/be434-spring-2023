83a1dc79f1d8353488ba1f047fa08877969231fc
jmazzone11 <jmazzone@arizona.edu>
2023-03-02 07:08:14 -0700

    practice

--- practice/practice.py
+++ practice/practice.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python3
-word = 'OCTOPUS'
-word[0].lower() in 'aeiou'
+"""
+Author : jmazzone <jmazzone@localhost>
+Date   : 2023-02-25
+Purpose: practice
+"""
 
-print(word)
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='practice',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    
+    parser.add_argument('-f',
+                        '--file',
+                        help='Input file',
+                        metavar='str',
+                        type=argparse.FileType('r'),
+                        default='wordcount.txt')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    x = args.file
+
+    print(type(x))
+    
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
